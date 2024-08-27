66598ce066cfc17f27c27994cbbb1e60f9a9a5da
kat coveney <katcoveney.author@gmail.com>
2024-08-27 15:55:00 -0500

    fixed errors fo the view all tags

--- views/tags.py
+++ views/tags.py
@@ -4,10 +4,11 @@ def get_all_tags():
     conn = sqlite3.connect('./db.sqlite3')
     cursor = conn.cursor()
 
-    cursor.execute("SELECT label FROM Tags ORDER BY label ASC")
+    # Select both id and label from the Tags table
+    cursor.execute("SELECT id, label FROM Tags ORDER BY label ASC")
     tags = cursor.fetchall()
 
     conn.close()
 
-    # Convert the list of tuples to a list of strings
-    return [tag[0] for tag in tags]
+    # Convert the list of tuples to a list of dictionaries with id and label
+    return [{"id": tag[0], "label": tag[1]} for tag in tags]
