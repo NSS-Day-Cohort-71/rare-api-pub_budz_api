9080b146b6f01eaf82d39af666511d9a134870a6
kat coveney <katcoveney.author@gmail.com>
2024-09-04 14:01:41 -0500

    added backend ability to view user posts

--- views/__init__.py
+++ views/__init__.py
@@ -1,5 +1,5 @@
 from .user import login_user, create_user, get_all_users, get_single_user
 from .tags import get_all_tags, edit_tag_view, get_tag_by_id, update_tag, delete_tag, create_tag
 from .categories import get_categories, update_category, delete_category, create_category
-from .posts import get_posts, get_single_post, update_post, delete_post, create_post
+from .posts import get_posts, get_single_post, update_post, delete_post, create_post, get_posts_by_user, update_post_approval
 from .comments import get_all_comments, delete_comment, create_comment, update_comment, get_comments_by_post
